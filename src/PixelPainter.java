3cc4ec1de68122b908fbc615addf1bd74ab5ca64
Jan Kotynia <jsiek.kotynia@gmail.com>
2024-07-13 18:31:11 +0200

    Add random particle spawn and random piling

--- src/PixelPainter.java
+++ src/PixelPainter.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
 
 public class PixelPainter extends JPanel {
     private static final int WIDTH = 200;
@@ -13,7 +14,7 @@ public class PixelPainter extends JPanel {
     public PixelPainter() {
         pixels = new boolean[WIDTH][HEIGHT];
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        setBackground(Color.BLACK);
+        setBackground(Color.WHITE);
 
     MouseAdapter mouseAdapter = new MouseAdapter() {
         @Override
@@ -27,11 +28,16 @@ public class PixelPainter extends JPanel {
         }
 
         private void drawPixel(int x, int y) {
+            int r1;
+            int r2;
+            Random random = new Random();
             if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
-                for(int i=6;i>0;i--)
+                for(int i=0;i<7;i++)
                 {
-                        if(x+i < WIDTH && y+i < HEIGHT)
-                            pixels[x+i][y+i]=true;
+                    r1 =random.nextInt((6 - (-6) + 1)) + (-6);
+                    r2 =random.nextInt((6 - (-6) + 1)) + (-6);
+                        if(x+r1 < WIDTH && y+r2 < HEIGHT)
+                            pixels[x+r1][y+r2]=true;
                 }
                 repaint();
             }
@@ -49,11 +55,28 @@ public class PixelPainter extends JPanel {
 }
 
     private void Physics() {
-        for (int i = WIDTH - 1; i > 0; i--) {
+        Random random = new Random();
+        int r;
+        for (int i = WIDTH - 2; i > 0; i--) {
             for (int j = HEIGHT - 2; j > 0; j--) {
                 if (pixels[i][j] && !pixels[i][j + 1]) {
                     pixels[i][j] = false;
                     pixels[i][j + 1] = true;
+
+                }
+            }
+        }
+
+        for (int i = WIDTH - 2; i > 0; i--) {
+            for (int j = HEIGHT - 2; j > 0; j--) {
+                if (pixels[i][j + 1] && !pixels[i + 1][j + 1] && !pixels[i - 1][j + 1]) {
+                    r = random.nextInt(2) + 1;
+                    pixels[i][j]=false;
+                    if (r == 1) {
+                        pixels[i + 1][j + 1] = true;
+                    } else {
+                        pixels[i - 1][j + 1] = true;
+                    }
                 }
             }
         }
@@ -66,7 +89,7 @@ public class PixelPainter extends JPanel {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 if (pixels[x][y]) {
-                    g.setColor(Color.WHITE);
+                    g.setColor(Color.BLACK);
                     g.drawLine(x, y, x, y);
                 }
             }
